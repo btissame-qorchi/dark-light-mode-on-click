1e679803a96b326210bfde7fc3ebb70b73847c9c
Btissameq <qorchi.btissam@gmail.com>
2021-01-09 16:02:44 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -37,27 +37,4 @@ $(document).ready(function(){
 
   //Dark/Light mode code
 
-  function themeDarkLight(){
-
-    const date = new Date();
-    const hour = date.getHours();
-
-    if(hour > 5 || hour < 19){
-      //We can add a class to the body to work the styles of the elements for each mode (light)
-      $('body').addClass('light_mode');
-      //Or modify element style directly in js with Css variables
-      document.documentElement.style.setProperty('--color','#000');
-      document.documentElement.style.setProperty('--bodyBg','#e4e4e4');
-      document.documentElement.style.setProperty('--containerBg','#fff');
-    }else{
-       //We can add a class to the body to work the styles of the elements for each mode (dark)
-      $('body').addClass('dark_mode');
-      //Or modify element style directly in js with Css variables
-      document.documentElement.style.setProperty('--color','#e4e4e4');
-      document.documentElement.style.setProperty('--bodyBg','#000');
-      document.documentElement.style.setProperty('--containerBg','#353535');
-    }
-  }
-  themeDarkLight();
-
 });
